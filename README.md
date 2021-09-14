a1027e52c7e110dd0e58e0e80ab12685336ae2ec
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2021-09-14 11:30:41 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,103 @@
+# SIF Tilgangskontroll
+
+![CI / CD](https://github.com/navikt/sif-tilgangskontroll/workflows/CI%20/%20CD/badge.svg)
+![Alerts](https://github.com/navikt/sif-tilgangskontroll/workflows/Alerts/badge.svg)
+![Vulnerabilities scanning of dependencies](https://github.com/navikt/sif-tilgangskontroll/workflows/Vulnerabilities%20scanning%20of%20dependencies/badge.svg)
+
+# Innholdsoversikt
+* [1. Kontekst](#1-kontekst)
+* [2. Funksjonelle Krav](#2-funksjonelle-krav)
+* [3. Begrensninger](#3-begrensninger)
+* [4. Programvarearkitektur](#5-programvarearkitektur)
+* [5. Kode](#6-kode)
+* [6. Data](#7-data)
+* [7. Infrastrukturarkitektur](#8-infrastrukturarkitektur)
+* [8. Distribusjon av tjenesten (deployment)](#9-distribusjon-av-tjenesten-deployment)
+* [9. Utviklingsmiljø](#10-utviklingsmilj)
+* [10. Drift og støtte](#11-drift-og-sttte)
+
+# 1. Kontekst
+Utføre tilgangskontroll av sluttbrukere og saksbehandlere for tjenester i sykdom-i-familien (SIF).
+
+# 2. Funksjonelle Krav
+Denne tjenesten understøtter behovet for utføring av tilgangskontroll av sluttbrukere og saksbehandlere for team sif.
+Tjenesten utfører tilgangskontroll basert på visse regelsett avhengig om bruker er borger eller saksbehandler.
+Tjenesten eksponerer api for at andre tjenester kan utføre tilgangskontroll på vegne av bruker.
+
+# 3. Begrensninger
+N/A
+
+# 4. ProgramvarearkitekN/A
+
+# 5. Kode
+N/A
+
+# 6. Data
+N/A
+
+# 7. Infrastrukturarkitektur
+N/A
+
+# 8. Distribusjon av tjenesten (deployment)
+Distribusjon av tjenesten er gjort med bruk av Github Actions.
+[SIF Tilgangskontroll API CI / CD](https://github.com/navikt/sif-tilgangskontroll/actions)
+
+Push/merge til master branche vil teste, bygge og deploye til produksjonsmiljø og testmiljø.
+
+# 9. Utviklingsmiljø
+## Forutsetninger
+* docker
+* Java 11
+* Kubectl
+
+## Bygge Prosjekt
+For å bygge kode, kjør:
+
+```shell script
+./gradlew clean build
+```
+
+## Kjøre Prosjekt
+For å kjøre kode, kjør:
+
+```shell script
+./gradlew bootRun
+```
+
+### Henting av data via api-endepunktene
+TODO: Forklar henting av gyldig token for bruk av apiet.
+
+# 10. Drift og støtte
+## Logging
+Loggene til tjenesten kan leses på to måter:
+
+### Kibana
+TODO: Legg til lenker for logg i kibana.
+
+### Kubectl
+For dev-gcp:
+```shell script
+kubectl config use-context dev-gcp
+kubectl get pods -n dusseldorf | grep sif-tilgangskontroll
+kubectl logs -f sif-tilgangskontroll-<POD-ID> --namespace dusseldorf -c sif-tilgangskontroll
+```
+
+For prod-gcp:
+```shell script
+kubectl config use-context prod-gcp
+kubectl get pods -n dusseldorf | grep sif-tilgangskontroll
+kubectl logs -f sif-tilgangskontroll-<POD-ID> --namespace dusseldorf -c sif-tilgangskontroll
+```
+
+## Alarmer
+Vi bruker [nais-alerts](https://doc.nais.io/observability/alerts) for å sette opp alarmer. Disse finner man konfigurert i [nais/alerterator-prod.yml](nais/alerterator-prod.yml).
+
+## Metrics
+N/A
+
+## Henvendelser
+Spørsmål koden eller prosjekttet kan rettes til team brukerdialog på:
+* [\#sif-brukerdialog](https://nav-it.slack.com/archives/CQ7QKSHJR)
+* [\#sif-innsynsplattform](https://nav-it.slack.com/archives/C013ZJTKUNB)
+
+
---
+++ build.gradle.kts
@@ -0,0 +1,117 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("org.springframework.boot") version "2.5.4"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    kotlin("jvm") version "1.5.30"
+    kotlin("plugin.spring") version "1.5.30"
+    kotlin("plugin.jpa") version "1.5.30"
+}
+
+group = "no.nav"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+val springfoxVersion by extra("3.0.0")
+val logstashLogbackEncoderVersion by extra("6.6")
+val tokenSupportVersion by extra("1.3.8")
+val springCloudVersion by extra("2020.0.3")
+val retryVersion by extra("1.3.0")
+val zalandoVersion by extra("0.26.2")
+val awailitilityKotlinVersion by extra("4.1.0")
+val assertkJvmVersion by extra("0.24")
+val springMockkVersion by extra("3.0.1")
+val mockkVersion by extra("1.11.0")
+val guavaVersion by extra("23.0")
+val okHttp3Version by extra("4.9.1")
+val orgJsonVersion by extra("20210307")
+
+ext["okhttp3.version"] = okHttp3Version
+
+repositories {
+    mavenCentral()
+
+    maven {
+        name = "confluent"
+        url = uri("https://packages.confluent.io/maven/")
+    }
+}
+
+dependencies {
+
+    // NAV
+    implementation("no.nav.security:token-validation-spring:$tokenSupportVersion")
+    implementation("no.nav.security:token-client-spring:$tokenSupportVersion")
+
+    testImplementation("no.nav.security:token-validation-spring-test:$tokenSupportVersion")
+    testImplementation("com.squareup.okhttp3:okhttp:$okHttp3Version")
+
+    // Spring Boot
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-web") {
+        exclude(group = "org.springframework.boot", module = "spring-boot-starter-tomcat")
+    }
+    implementation("org.springframework.boot:spring-boot-starter-jetty")
+    implementation("org.springframework.retry:spring-retry:$retryVersion")
+    implementation("org.springframework:spring-aspects")
+    runtimeOnly("org.springframework.boot:spring-boot-properties-migrator")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+    annotationProcessor("org.springframework.boot:spring-boot-autoconfigure-processor")
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude(group = "junit")
+        exclude(module = "mockito-core")
+    }
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine")
+
+    // SpringFox
+    implementation("io.springfox:springfox-boot-starter:$springfoxVersion")
+
+    // Metrics
+    implementation("io.micrometer:micrometer-registry-prometheus")
+
+    // Logging
+    implementation("net.logstash.logback:logstash-logback-encoder:$logstashLogbackEncoderVersion")
+
+    // Jackson
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+    // Kotlin
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+
+    // Diverse
+    implementation("org.json:json:$orgJsonVersion")
+    implementation("com.github.ben-manes.caffeine:caffeine")
+    implementation("org.zalando:problem-spring-web-starter:$zalandoVersion")
+    implementation("com.google.guava:guava:$guavaVersion")
+    testImplementation("org.awaitility:awaitility-kotlin:$awailitilityKotlinVersion")
+    testImplementation("com.willowtreeapps.assertk:assertk-jvm:$assertkJvmVersion")
+    testImplementation("com.ninja-squad:springmockk:$springMockkVersion")
+    testImplementation("io.mockk:mockk:$mockkVersion")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "11"
+    }
+}
+
+tasks.getByName<Jar>("jar") {
+    enabled = false
+}
