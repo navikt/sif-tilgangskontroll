fdbcd61d7ec6d51c74af75a2a119ca1a3f5ebb3a
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2022-06-10 08:52:30 +0200

    Oppgraderer gradle kotlin plugins.
    - cloaes #59
    - cloaes #58

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,8 @@ plugins {
     `maven-publish`
     id("org.springframework.boot") version "2.7.0"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    kotlin("jvm") version "1.6.21"
-    kotlin("plugin.spring") version "1.6.21"
+    kotlin("jvm") version "1.7.0"
+    kotlin("plugin.spring") version "1.7.0"
 }
 
 java.sourceCompatibility = JavaVersion.VERSION_17
