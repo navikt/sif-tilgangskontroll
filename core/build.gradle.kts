aa37f15c548d2e8876ff91b87e53248880096b8d
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2022-08-03 10:03:08 +0200

    Oppgraderer pakker og plugins.
    - closes #70
    - closes #68
    - closes #65
    - closes #64
    - closes #63
    - oppgraderer token-support
    - oppgraderer spring retry
    - oppgraderer mockK
    - oppgraderer okHttp3

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,12 +1,12 @@
 
 plugins {
     kotlin("jvm")
-    id("com.expediagroup.graphql")  version "6.0.0"
+    id("com.expediagroup.graphql")  version "6.1.0"
 }
 
-val tokenSupportVersion by extra("2.0.14")
-val okHttp3Version by extra("4.9.1")
-val graphQLKotlinVersion by extra("6.0.0")
+val tokenSupportVersion by extra("2.1.2")
+val okHttp3Version by extra("4.10.1")
+val graphQLKotlinVersion by extra("6.1.0")
 
 ext["okhttp3.version"] = okHttp3Version
 
