c95582a24e7eea34a783a71c9e8e77acc85e0102
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2022-12-20 14:49:20 +0100

    Oppgraderer til Spring Boot 3.0.
    Oppdaterer pakker og plugins.

--- src/main/kotlin/no/nav/siftilgangskontroll/filter/HeadersToMDCFilterBean.kt
+++ src/main/kotlin/no/nav/siftilgangskontroll/filter/HeadersToMDCFilterBean.kt
@@ -10,11 +10,11 @@ import org.springframework.core.annotation.Order
 import org.springframework.stereotype.Component
 import org.springframework.web.filter.GenericFilterBean
 import java.io.IOException
-import javax.servlet.FilterChain
-import javax.servlet.ServletException
-import javax.servlet.ServletRequest
-import javax.servlet.ServletResponse
-import javax.servlet.http.HttpServletRequest
+import jakarta.servlet.FilterChain
+import jakarta.servlet.ServletException
+import jakarta.servlet.ServletRequest
+import jakarta.servlet.ServletResponse
+import jakarta.servlet.http.HttpServletRequest
 
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
