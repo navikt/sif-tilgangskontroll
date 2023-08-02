3edd809d4b4c23653194579a8ced45fcff194fe3
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2023-08-02 12:55:28 +0200

    Oppgraderer gradle wrapper.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,17 @@
 rootProject.name = "sif-tilgangskontroll"
 
 include("spesification", "core")
+
+plugins {
+    id("com.gradle.enterprise") version("3.13.4")
+}
+
+gradleEnterprise {
+    if (System.getenv("CI") != null) {
+        buildScan {
+            publishAlways()
+            termsOfServiceUrl = "https://gradle.com/terms-of-service"
+            termsOfServiceAgree = "yes"
+        }
+    }
+}
