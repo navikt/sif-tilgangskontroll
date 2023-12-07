f388bfe32c94d7483dc8d017c38b90e47cde6178
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2023-12-07 10:09:26 +0100

    Bumper token-support og graphql

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -4,9 +4,9 @@ plugins {
     id("com.expediagroup.graphql") version "7.0.2"
 }
 
-val tokenSupportVersion by extra("3.1.0")
+val tokenSupportVersion by extra("3.2.0")
 
-val graphQLKotlinVersion by extra("7.0.1")
+val graphQLKotlinVersion by extra("7.0.2")
 
 dependencies {
     implementation(project(":spesification"))
